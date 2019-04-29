e28e2ba9c9930ec49ef529a92957f09a2b8b380c
Raymond Butcher <ray.butcher@claranet.uk>
2019-04-29 14:50:18 +0100

    Release 0.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='boto-source-profile-mfa',
-    version='0.0.3',
+    version='0.0.4',
     description='AWS boto helper library for reusing MFA tokens in profiles with the same source profile',
     author='Raymond Butcher',
     author_email='ray.butcher@claranet.uk',
